29548e9649daca1e89256e0a4c010f1a4d815783
kuschti <kuschti@gmail.com>
2014-01-02 18:02:23 +0100

    add theme support functions to plugin

--- riitstoebli-setup/riitstoebli-setup-plugin.php
+++ riitstoebli-setup/riitstoebli-setup-plugin.php
@@ -161,6 +161,52 @@ add_action( 'init', 'cpt_rs_eventtypes_init');
         https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress
     */
     
+    // wp thumbnails (sizes handled in functions.php)
+    add_theme_support('post-thumbnails');
+
+    // default thumb size
+    set_post_thumbnail_size(125, 125, true);
+
+    // wp custom background (thx to @bransonwerner for update)
+    add_theme_support( 'custom-background',
+        array(
+        'default-image' => '',  // background image default
+        'default-color' => '', // background color default (dont add the #)
+        'wp-head-callback' => '_custom_background_cb',
+        'admin-head-callback' => '',
+        'admin-preview-callback' => ''
+        )
+    );
+
+    // rss thingy
+    add_theme_support('automatic-feed-links');
+
+    // to add header image support go here: http://themble.com/support/adding-header-background-image-support/
+
+    // adding post format support
+    add_theme_support( 'post-formats',
+        array(
+            'aside',             // title less blurb
+            'gallery',           // gallery of images
+            'link',              // quick link to other site
+            'image',             // an image
+            'quote',             // a quick quote
+            'status',            // a Facebook like status update
+            'video',             // video
+            'chat'               // chat transcript
+        )
+    );
+
+    // wp menus
+    add_theme_support( 'menus' );
+
+    // registering wp3+ menus
+    register_nav_menus(
+        array(
+            'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
+            'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
+        )
+    );
 
 /* Stop Adding Functions Below this Line */
 ?>
